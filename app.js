772da9bd668613b26fe9c7f02d8efecd6817112d
Bouziane <bey.bouziane@live.fr>
2020-09-16 10:25:22 +0200

    Update folders

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
 const hbs = require("hbs");
+
+//TEMPLATE CONFIG
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 hbs.registerPartials(__dirname + "/views/partials");
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("main");
+//ROUTING
+app.get("/home", (req, res) => {
+  res.render("home");
 });
 
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.get("/home", (req, res) => {
-  res.render("home");
-});
-
+//KICKSTART PHASE
 app.listen(port, () => {
   console.log(port);
 });
